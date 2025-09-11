ead5508b573524918909ec94343ad0b6bdbb2eef
Aksel <akselbektas@yahoo.co.uk>
2025-09-11 16:01:36 +0200

    Change Wordpress Export to Firebase

--- backend/3.1 export_to_firebase.py
+++ backend/3.1 export_to_firebase.py
@@ -1,48 +1,35 @@
 import json
 import requests
 import os
-from setup import *
+import firebase_admin
+from setup import initialize_firebase, RESULTS_DATE_PATH
 
-# SETUP
-wp_site_url = "efunero.com"
-custom_post_type = "funeral_provider"
 
-access_token = 'oaJB$l2DmRTnn8i$V0PKe9bPcq#jXxUm9sNX0&KVI@J)MO*QOsNr*FtX4JYfOc)t'
 
-#os.getenv("WP_ACCESS_TOKEN")
+funeral_provider_export_path = f"{RESULTS_DATE_PATH}/cleaned/final_firebase_export_version.json"
 
-# WordPress.com REST API endpoint
-endpoint = f"https://public-api.wordpress.com/rest/v1.1/sites/{wp_site_url}/posts/new"
 
-funeral_provider_export_path = f"{PROJECT_ROOT}/results/wordpress/funeral_providers_export_{DATE}.json"
+# Define function for uploading data to firebase
+def upload_funeral_data(data : dict):
+    db = initialize_firebase()
+    
+
+    try:
+        # Batch write for better performance
+        batch = db.batch()
+        
+        for item in data:
+            # Create a reference with auto-generated ID
+            doc_ref = db.collection("funeral_providers").document()
+            batch.set(doc_ref, item)
+        
+        # Commit the batch
+        batch.commit()
+    finally:
+        firebase_admin.delete_app(firebase_admin.get_app())
 
 # Load JSON data
 with open(funeral_provider_export_path, "r", encoding="utf-8") as f:
     funeral_data = json.load(f)
 
-# Post each funeral provider
-for entry in funeral_data:
-    # Format for WordPress.com REST API
-    post_data = {
-        "title": entry["funeral_provider"],
-        "status": "publish",
-        #"content": f"Location: {entry['location']}, Price: €{entry['price']}, Date: {entry['date']}",
-        "type": "funeral_provider",
-        "metadata": [
-            {"key": "name_of_provider", "value": entry["funeral_provider"]},
-            {"key": "url_field", "value": entry["url"]},
-            {"key": "location_field", "value": entry["location"]},
-            {"key": "price_field", "value": entry["price"]},
-            {"key": "date_field", "value": entry["date"]}
-        ]
-    }
-
-    # Make request using the access token
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-    
-    response = requests.post(endpoint, headers=headers, json=post_data)
-
-    print(1)
+    upload_funeral_data(funeral_data)
--- backend/setup.py
+++ backend/setup.py
@@ -1,11 +1,12 @@
 # SETUP 
 import os
+from firebase_admin import credentials, firestore, initialize_app
 
 PROJECT_ROOT = "C:/Projects/Efunero/backend"
 DATE = "2025_09_08"
 
-USERNAME = "akselbektas@yahoo.co.uk"
-APPLICATION_PASSWORD = "QBTO jQkr UaNS eo2C jK6K 7pt6"
+firebase_config_path = f"{PROJECT_ROOT}/resources/efunero-firebase-adminsdk-fbsvc-c3659522ea.json"
+
 PROBLEMATIC_FUNERAL_PROVIDERS = ['Hautaustoimisto Toro']
 
 # SETUP
@@ -14,4 +15,8 @@ RESULTS_DATE_PATH = f"{RESULTS_DIR}/{DATE}"
 FUNERAL_DIRECTOR_NAMES = [x for x in os.listdir(RESULTS_DATE_PATH) if x != "cleaned"]
 
 
+def initialize_firebase():
+    cred = credentials.Certificate(firebase_config_path)
+    initialize_app(cred)
+    return firestore.client()
 
